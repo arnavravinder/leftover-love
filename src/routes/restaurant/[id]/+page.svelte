b7a9c96a223dbc5843e56d9f99537a37dd210532
Nahos <zsaquar1an.42@gmail.com>
2024-07-05 12:13:14 +0530

    add cart functionality

--- src/routes/restaurant/[id]/+page.svelte
+++ src/routes/restaurant/[id]/+page.svelte
@@ -2,14 +2,19 @@
 	import { db } from '$lib/firebase.client.js';
 	import type { Item } from '$lib/item.js';
 	import type { Restaurant } from '$lib/restaurant.js';
+	import { session } from '$lib/session';
+	import { update } from 'firebase/database';
 	import {
+		arrayUnion,
 		collection,
 		doc,
+		FieldValue,
 		getDoc,
 		getDocs,
 		getFirestore,
 		query,
 		Timestamp,
+		updateDoc,
 		where
 	} from 'firebase/firestore';
 	import { onMount } from 'svelte';
@@ -19,7 +24,7 @@
 	let items: Item[] = [];
 
 	onMount(async () => {
-		restaurant = (await getDoc(doc(getFirestore(), 'restaurants', data.id))).data() as Restaurant;
+		restaurant = (await getDoc(doc(db, 'restaurants', data.id))).data() as Restaurant;
 		const q = await getDocs(query(collection(db, 'items'), where('restaurantId', '==', data.id)));
 		q.forEach((val) => {
 			console.log(val.data());
@@ -34,7 +39,18 @@
 	{#each items as item}
 		<h2>{item.name}</h2>
 		<p>{item.type}</p>
-		<button>Add to cart</button>
+		<button
+			on:click={async () => {
+				console.log($session?.user?.uid);
+				if ($session?.user?.uid) {
+					const cartDoc = doc(db, 'cart', $session?.user?.uid);
+					const cartSnapshot = await getDoc(cartDoc);
+					if (cartSnapshot.exists()) {
+						await updateDoc(cartDoc, { items: arrayUnion(item) });
+					}
+				}
+			}}>Add to cart</button
+		>
 	{/each}
 {:else}
 	<h1>Loading...</h1>
